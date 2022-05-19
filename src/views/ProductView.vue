a09342423f30a0d897fc83c7f94b5a28943a7953
sgarcal <sgarcal763@gmail.com>
2022-05-20 01:15:37 +0200

    paypal fix 2 try lang ts

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -1,30 +1,23 @@
 <template>
   <div class="main">
     <div class="name-p">
-     
+      {{ producto.nombre }}
     </div>
     <div class="top-p">
       <div class="img-p">
         <img />
       </div>
       <div class="price-p">
-          <div class="precio">
-             
-          </div>
-          <div class="cantidad">
-            <span>Cantidad:</span><input type="number" />
-          </div>
-          <div class="total">
-
-          </div>
-
+        <div class="precio">{{ producto.precio }}</div>
+        <div class="cantidad">
+          <span>Cantidad:</span><input type="number" v-model="cantidad"/>
+        </div>
+        <div class="total">{{cantidad}}</div>
       </div>
     </div>
     <div class="bot-p">
       <div class="desc-p">
-          <p class="desc">
-             
-          </p>
+        <p class="desc"></p>
       </div>
       <div class="coments-p"></div>
     </div>
@@ -32,28 +25,79 @@
   <div ref="paypal"></div>
 </template>
 
-<script>export default {
-  name: "HelloWorld",
+<script lang="ts">
+import Swal from 'sweetalert2';
+import { useRoute } from 'vue-router';
+import { RoutePaths } from '../core/general/RoutePaths';
+import axios from "axios";
+import { ref } from "vue";
 
-  data: function() {
+export default {
+  name: "ProductView",
+
+  data() {
+      
     return {
       loaded: false,
       paidFor: false,
-      product: {
-        price: 777.77,
-        description: "leg lamp from that one movie",
-        img: "./assets/lamp.jpg"
+      cantidad: ref<Number>(0),
+      producto: {
+        nombre: "",
+        precio: 0,
+        description: "",
+
       }
     };
   },
-  mounted: function() {
+  mounted() {
     const script = document.createElement("script");
     script.src =
       "https://www.paypal.com/sdk/js?client-id=AY-sBQ9Vftv20_Ys3NGCJyJ_2jCPp1NxO7-sinxBniEWwCqK5Uf_aJm-vLzYbl2y9ok4cuxAFb4Vm2AR";
     script.addEventListener("load", this.setLoaded);
     document.body.appendChild(script);
+
+    this.getProductById();
+
   },
   methods: {
+
+    getProductById: function() {
+        this.producto.precio = 9
+
+        const formData = new FormData();
+        formData.append('id', this.$route.params.id)
+
+         axios.post(`${RoutePaths.API}getProductById.php`, formData)
+        .then((response) => {
+          switch (response.status) {
+            case 200:
+                this.producto.precio = response.data.response.prodprec
+              break;
+
+            case 404:
+              Swal.fire({
+                icon: "error",
+                title: "ERROR",
+                text: "Error interno. No se ha encontrado la ruta",
+                showConfirmButton: false,
+                timer: 2000,
+              });
+              break;
+
+            case 500:
+              Swal.fire({
+                icon: "error",
+                title: "ERROR",
+                text: "Error interno. Fallo de API",
+                showConfirmButton: false,
+                timer: 2000,
+              });
+              break;
+          }
+        }).catch((error) => {
+          console.error("There was an error!", error);
+        });
+    },
     setLoaded: function() {
       this.loaded = true;
       window.paypal
@@ -65,16 +109,21 @@
                   description: this.product.description,
                   amount: {
                     currency_code: "USD",
-                    value: this.product.price
+                    value: 20
                   }
                 }
               ]
             });
           },
           onApprove: async (data, actions) => {
-            const order = await actions.order.capture();
-            this.paidFor = true;
-            console.log(order);
+            // const order = await actions.order.capture();
+            // this.paidFor = true;
+            // console.log(order);
+            Swal.fire({
+                  icon: "success",
+                  title: "Correcto",
+                  showConfirmButton: true,
+                });
           },
           onError: err => {
             console.log(err);
@@ -82,6 +131,7 @@
         })
         .render(this.$refs.paypal);
     }
+
   }
 };
 </script>
