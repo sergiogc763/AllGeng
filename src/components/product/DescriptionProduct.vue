00667e8206d845fe56fb12e22fe6bb0b2cfb8d94
sgarcal <sgarcal763@gmail.com>
2022-05-20 12:55:58 +0200

    Add options product view

---
+++ src/components/product/DescriptionProduct.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="descripcion">
+    <p>{{ props.descripcion }}</p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+  descripcion: {
+    required: true,
+    type: String,
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.descripcion {
+
+    p{
+        margin-top: 25px;
+        margin-right: 25vw;
+        margin-left: 25vw;
+    }
+  
+}
+</style>
--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -1,52 +1,76 @@
 <template>
   <div class="main">
-    <div class="name-p">
-      {{ producto.nombre }}
-    </div>
+    <div class="banner"></div>
     <div class="top-p">
       <div class="img-p">
-        <img />
+        <img src="@/assets/n64.jpg" style="width: 75%;"/>
       </div>
       <div class="price-p">
-        <div class="precio">{{ producto.precio }}</div>
-        <div class="cantidad">
-          <span>Cantidad:</span><input @change="onChange($event)" type="number"/>
+        <div class="nombre">
+          <h5>{{ producto.nombre }}</h5>
         </div>
-        <div class="total"></div>
+        <div class="precio">{{ producto.precio }} €</div>
+        <div class="cantidad">
+          <span>Cantidad:</span><input @change="onChange($event)" type="number" min="1" />
+        </div>
+        <div class="total">Precio total: {{ total }} €</div>
+        <div ref="paypal"></div>
       </div>
     </div>
+    <div class="banner"></div>
     <div class="bot-p">
-      <div class="desc-p">
-        <p class="desc"></p>
+      <ul class="nav nav-tabs">
+        <li class="nav-item" >
+          <a class="nav-link" @click="optionSelected(1)">Descripción</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" @click="optionSelected(2)">Características</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" @click="optionSelected(3)">Comentarios</a>
+        </li>
+      </ul>
+      <div class="option-show">
+        <div class="descripcion" v-if="opcion === 1">
+            <DescriptionProduct :descripcion="producto.descripcion"/>
+        </div>
+        <div class="caracterisitcas" v-if="opcion === 2">
+            <p>{{producto.descripcion}}</p>
+        </div>
+        <div class="comentarios" v-if="opcion === 3">
+            <p>{{producto.descripcion}}</p>
+        </div>
       </div>
-      <div class="coments-p"></div>
+       
     </div>
   </div>
-  <div ref="paypal"></div>
 </template>
 
-<script lang="ts">
-import Swal from 'sweetalert2';
-import { useRoute } from 'vue-router';
-import { RoutePaths } from '../core/general/RoutePaths';
-import axios from "axios";
+<script>
+import Swal from "sweetalert2";
+import { RoutePaths } from "../core/general/RoutePaths";
 import { ref } from "vue";
+import DescriptionProduct from "@/components/product/DescriptionProduct.vue";
+import axios from "axios";
 
 export default {
   name: "ProductView",
-
+  components: {
+    DescriptionProduct
+  },
   data() {
-      
     return {
       loaded: false,
       paidFor: false,
       cantidad: 0,
+      total: 0,
       producto: {
         nombre: "",
+        img: "",
         precio: 0,
-        description: "",
-
-      }
+        descripcion: "",
+      },
+      opcion: 0
     };
   },
   mounted() {
@@ -57,26 +81,30 @@ export default {
     document.body.appendChild(script);
 
     this.getProductById();
-
   },
   methods: {
-
+    optionSelected(option){
+      this.opcion = option
+    },
     onChange(event) {
-            console.log(event.target.value)
-            this.cantidad = event.target.value;
-            console.log(this.cantidad);
-        },
-    getProductById: function() {
-        this.producto.precio = 9
+      this.cantidad = event.target.value;
+      this.total = this.cantidad * this.producto.precio;
+    },
+    getProductById() {
+      this.producto.precio = 9;
 
-        const formData = new FormData();
-        formData.append('id', this.$route.params.id)
+      const formData = new FormData();
+      formData.append("id", this.$route.params.id);
 
-         axios.post(`${RoutePaths.API}getProductById.php`, formData)
+      axios
+        .post(`${RoutePaths.API}getProductById.php`, formData)
         .then((response) => {
           switch (response.status) {
             case 200:
-                this.producto.precio = response.data.response.prodprec
+              this.producto.nombre = response.data.response.prodnom;
+              this.producto.precio = response.data.response.prodprec;
+              this.producto.descripcion = response.data.response.proddesc;
+              this.total = this.producto.precio;
               break;
 
             case 404:
@@ -99,11 +127,12 @@ export default {
               });
               break;
           }
-        }).catch((error) => {
+        })
+        .catch((error) => {
           console.error("There was an error!", error);
         });
     },
-    setLoaded: function() {
+    setLoaded() {
       this.loaded = true;
       window.paypal
         .Buttons({
@@ -111,13 +140,13 @@ export default {
             return actions.order.create({
               purchase_units: [
                 {
-                  description: this.product.description,
+                  description: this.producto.nombre,
                   amount: {
                     currency_code: "USD",
-                    value: 20
-                  }
-                }
-              ]
+                    value: this.total,
+                  },
+                },
+              ],
             });
           },
           onApprove: async (data, actions) => {
@@ -125,20 +154,61 @@ export default {
             // this.paidFor = true;
             // console.log(order);
             Swal.fire({
-                  icon: "success",
-                  title: "Correcto",
-                  showConfirmButton: true,
-                });
+              icon: "success",
+              title: "Compra realizada",
+              text: "La compra se a efectuado correctamente. ¡Muchas gracias!",
+              showConfirmButton: true,
+            });
           },
-          onError: err => {
+          onError: (err) => {
             console.log(err);
-          }
+          },
         })
         .render(this.$refs.paypal);
-    }
-
-  }
+    },
+  },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.main {
+  background-color: white;
+
+  .banner {
+    margin: 0;
+    width: 100%;
+    background-color: black;
+    height: 50px;
+  }
+  .top-p {
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    .price-p {
+      // margin-top: 15vh;
+      background-color: rgb(183, 183, 183);
+      padding: 20px;
+      border-radius: 5px;
+      height: fit-content;
+      .nombre {
+        display: flex;
+        justify-content: center;
+      }
+
+      .precio{
+        color:rgb(255, 128, 0);
+        font-weight: bold;
+        font-size: 25px;
+      }
+    }
+  }
+
+  .bot-p{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    
+  }
+}
+</style>
