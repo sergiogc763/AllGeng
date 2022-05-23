da4d4a72eb3baca8aaae1029bf8b81ef6358853f
sgarcal <sgarcal763@gmail.com>
2022-05-23 21:19:30 +0200

    basic template upload product

--- src/views/UploadProductView.vue
+++ src/views/UploadProductView.vue
@@ -1,9 +1,7 @@
 <template>
-  <div class="content">
+  <div class="containt">
     <div class="mb-3">
-      <label for="nombre" class="form-label"
-        >Nombre:</label
-      >
+      <label for="nombre" class="form-label">Nombre:</label>
       <input
         type="text"
         class="form-control"
@@ -12,18 +10,42 @@
       />
     </div>
     <div class="mb-3">
-      <label for="descripcion" class="form-label"
-        >Descripción:</label
-      >
-      <textarea
-        class="form-control"
-        id="descripcion"
-        rows="3"
-      ></textarea>
+      <label for="precio" class="form-label">Precio:</label>
+      <input type="number" id="precio" step="0.01" min="0" value="1">
+    </div>
+    <div class="mb-3">
+      <label for="imagen" class="form-label">Imagen:</label>
+      <input type="file" id="imagen">
+    </div>
+    <div class="mb-5">
+      <label for="descripcion" class="form-label">Descripción:</label>
+      <textarea class="form-control" id="descripcion" rows="3"></textarea>
+    </div>
+    <div class="options mb-2">
+        <button type="button" class="btn btn-primary">
+            Añadir
+        </button>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup></script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.containt {
+  border-radius: 5px;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  background-color: rgb(201, 201, 201);
+  width: fit-content;
+  margin-top: 10px;
+  margin-bottom: 10px;
+
+  input,
+  textarea {
+    width: 25vw;
+  }
+
+}
+</style>
