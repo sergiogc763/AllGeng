9ac70eb05376890b69458c856c7a9f28b1d3e22b
sgarcal <sgarcal763@gmail.com>
2022-05-31 14:06:51 +0200

    define style product historial

--- src/views/UserHistorialView.vue
+++ src/views/UserHistorialView.vue
@@ -6,9 +6,7 @@
         v-model="fecha"
         @change="mostrarProductosFecha"
       >
-        <option selected value="3">
-          Últimos 3 meses
-        </option>
+        <option selected value="3">Últimos 3 meses</option>
         <option v-for="options in fechas" v-bind:value="options">
           {{ options }}
         </option>
@@ -19,11 +17,34 @@
       <div class="producto" v-for="p in historial">
         {{ p }}
 
-        <div class="top-banner"></div>
-        <div class="p-img">
-          <img class="img" :src="RoutePaths.BASE + p.imagen" />
+        <div class="top-banner">
+          <div class="datos-left">
+            <div class="fecha apartado">
+              <span class="titulo">PEDIDO REALIZADO</span>
+              <span>{{ p.histfecha }} a las {{ p.histhora }}</span>
+            </div>
+            <div class="total apartado">
+              <span class="titulo">TOTAL</span>
+              <span>{{ p.histprecio }}</span>
+            </div>
+            <div class="cantidad apartado">
+              <span class="titulo">CANTIDAD</span>
+              <span>{{ p.histcantidad }} uds</span>
+            </div>
+          </div>
+          <div class="datos-right">
+            <div class="numero apartado">
+              <span class="titulo">Nº DE PEDIDO</span>
+              <span>{{ p.histnumpedido }}</span>
+            </div>
+          </div>
         </div>
-        <div class="p-datos"></div>
+        <div class="content-p">
+          <div class="img-p">
+            <img :src="RoutePaths.BASE + p.imagen" />
+          </div>
+          <div class="datos-p"></div>
+        </div>
       </div>
     </div>
   </div>
@@ -35,25 +56,24 @@ import { RoutePaths } from "@/core/general/RoutePaths";
 import Page404 from "@/components/Page404.vue";
 import axios from "axios";
 import Swal from "sweetalert2";
-import { onBeforeMount, reactive, ref } from "vue";
+import { onBeforeMount, ref } from "vue";
 import { useStore } from "vuex";
 
-//#region CONST
-
-const fecha = ref<any>(3);
-
-const fechas = ref<Array<any>>([]);
-
+//#region CONST USE
 const store = useStore();
+//#endregion
 
+//#region REF
+const fecha = ref<any>(3);
+const fechas = ref<Array<any>>([]);
 const historial = ref<Array<any>>([]);
+//#endregion
 
 onBeforeMount(() => {
   //Lo primero que hacemos es comprobar que el usuario se encuentre logeado con éxito antes de realizar la obtención de los datos
   if (store.getters.logged) {
     const formData = new FormData();
     formData.append("id", store.getters.userId);
-    //   formData.append("filtro", filtro);
 
     axios
       .post(`${RoutePaths.API}getFechasHistorial.php`, formData)
@@ -68,7 +88,7 @@ onBeforeMount(() => {
 
             fechas.value = Array.from(new Set(fechasBD)); //Quitamos fechas duplicadas
 
-            obtenerHistorialFitro();//obtenemos historial principal con el filtro de 3 meses
+            obtenerHistorialFitro(); //obtenemos historial principal con el filtro de 3 meses
             break;
 
           case 404:
@@ -98,12 +118,12 @@ onBeforeMount(() => {
   }
 });
 
-
-
+//Función que se ejecuta cada vez que el select del filtro cambia
 function mostrarProductosFecha() {
- obtenerHistorialFitro();
+  obtenerHistorialFitro();
 }
 
+//Obtenemos el historial del usuario actual según el filtro indicado
 function obtenerHistorialFitro() {
   const formData = new FormData();
   formData.append("id", store.getters.userId);
@@ -114,7 +134,7 @@ function obtenerHistorialFitro() {
     .then((response) => {
       switch (response.status) {
         case 200:
-        historial.value = []
+          historial.value = [];
           response.data.products.data.forEach((element: any) => {
             historial.value.push(element);
           });
@@ -145,7 +165,6 @@ function obtenerHistorialFitro() {
       console.error("There was an error!", error);
     });
 }
-
 </script>
 
 <style lang="scss" scoped>
@@ -160,6 +179,39 @@ function obtenerHistorialFitro() {
     .producto {
       display: flex;
       flex-direction: column;
+
+      .top-banner {
+        background-color: rgb(198, 198, 198);
+        border-radius: 2px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+        .apartado {
+          display: flex;
+          flex-direction: column;
+          margin: 5px;
+        }
+        .datos-left {
+          display: flex;
+
+          div {
+            margin: 10px;
+          }
+        }
+      }
+
+      .content-p{
+          background-color: white;
+          border-radius: 2px;
+
+          .img-p{
+              img{
+                  width: 255px;
+                  height: 255px;
+              }
+          }
+      }
     }
   }
 }
