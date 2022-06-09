251cf887de3740779a14c50fc944acc2115e8896
sgarcal <sgarcal763@gmail.com>
2022-06-09 19:41:58 +0200

    add validaate upload product

--- src/core/general/RoutePaths.ts
+++ src/core/general/RoutePaths.ts
@@ -6,10 +6,12 @@ export enum RoutePaths {
     ProductView = "/producto/:nombre/:id",
     NewProduct = "/producto/nuevo",
     Historial = "/user/historial",
-    API = '/alumnado/curso2122/DAW/daw2122a5/API/',
-    BASE = '/alumnado/curso2122/DAW/daw2122a5'
-    // API = 'http://www.iestrassierra.net/alumnado/curso2122/DAW/daw2122a5/API/',
-    // BASE = 'http://www.iestrassierra.net/alumnado/curso2122/DAW/daw2122a5',
+
+
+    // API = '/alumnado/curso2122/DAW/daw2122a5/API/',
+   // BASE = '/alumnado/curso2122/DAW/daw2122a5'
+    API = 'http://www.iestrassierra.net/alumnado/curso2122/DAW/daw2122a5/API/',
+    BASE = 'http://www.iestrassierra.net/alumnado/curso2122/DAW/daw2122a5',
     // API = 'http://localhost/API/',
     // BASE = 'http://localhost'
 }
--- src/views/UploadProductView.vue
+++ src/views/UploadProductView.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="container tm-mt-big tm-mb-big" v-if="store.getters.logged && store.getters.rolId === RolUser.Gestor">
+  <div
+    class="container tm-mt-big tm-mb-big"
+    v-if="store.getters.logged && store.getters.rolId === RolUser.Gestor"
+  >
     <div class="row">
       <div class="col-xl-9 col-lg-10 col-md-12 col-sm-12 mx-auto">
         <div class="tm-bg-primary tm-block tm-block-h-auto">
@@ -12,97 +15,114 @@
           </div>
           <form class="row tm-edit-product-row" enctype="multipart/form-data">
             <div class="col-xl-6 col-lg-6 col-md-12">
-              <div
-                action=""
-                class="tm-edit-product-form"
-                enctype="multipart/form-data"
-              >
-                <div class="form-group mb-3">
-                  <label for="name">Nombre del prodructo </label>
-                  <input
-                    id="name"
-                    name="name"
-                    v-model="nombre"
-                    type="text"
-                    class="form-control"
-  
-                  />
-                </div>
-                <div class="form-group mb-3">
-                  <label for="description">Descripción</label>
-                  <textarea class="form-control" rows="50"></textarea>
-                </div>
-                <div class="form-group mb-3">
-                  <label for="categoria">Categoria</label>
-                  <select
+              <div class="form-group mb-3">
+                <label for="name">Product Name </label>
+                <input
+                  id="name"
+                  name="name"
+                  v-model="state.nombre"
+                  type="text"
+                  class="form-control"
+                />
+                <span class="error" v-if="v$.nombre.$error">
+                  {{ v$.nombre.$errors[0].$message }}
+                </span>
+              </div>
+              <div class="form-group mb-3">
+                <label for="description">Description</label>
+                <textarea class="form-control" v-model="state.descripcion" rows="50"></textarea>
+                <span class="error" v-if="v$.descripcion.$error">
+                  {{ v$.descripcion.$errors[0].$message }}
+                </span>
+              </div>
+              <div class="form-group mb-3">
+                <label for="categoria">Category</label>
+                <select
                   id="categoria"
-                    class="custom-select tm-select-accounts"
-                    v-model="categoria"
+                  class="custom-select tm-select-accounts"
+                  v-model="state.categoria"
+                >
+                  <option selected value disabled>Select a category</option>
+                  <option
+                    v-for="option in categorias"
+                    v-bind:value="option.value"
+                    v-bind:key="option"
                   >
-                    <option selected disabled>Seleccione una categoría</option>
-                    <option
-                      v-for="option in categorias"
-                      v-bind:value="option.value"
-                      v-bind:key="option"
-                    >
-                      {{ option.text }}
-                    </option>
-                  </select>
-                </div>
-                <div class="form-group mb-3">
-                  <label for="tipo">Tipo</label>
-                  <select
-                    id="tipo"
-                    class="custom-select tm-select-accounts"
-                    v-model="tipo"
+                    {{ option.text }}
+                  </option>
+                </select>
+                <span class="error" v-if="v$.categoria.$error">
+                  {{ v$.categoria.$errors[0].$message }}
+                </span>
+              </div>
+              <div class="form-group mb-3">
+                <label for="tipo">Type</label>
+                <select
+                  id="tipo"
+                  class="custom-select tm-select-accounts"
+                  v-model="state.tipo"
+                >
+                  <option selected value disabled>Select a type</option>
+                  <option
+                    v-for="option in tipos"
+                    v-bind:value="option.value"
+                    v-bind:key="option"
                   >
-                    <option selected disabled>Seleccione un tipo</option>
-                    <option
-                      v-for="option in tipos"
-                      v-bind:value="option.value"
-                      v-bind:key="option"
-                    >
-                      {{ option.text }}
-                    </option>
-                  </select>
+                    {{ option.text }}
+                  </option>
+                </select>
+                <span class="error" v-if="v$.tipo.$error">
+                  {{ v$.tipo.$errors[0].$message }}
+                </span>
+              </div>
+              <div class="form-group mb-3">
+                <label for="marca">Brand</label>
+                <select
+                  class="custom-select tm-select-accounts"
+                  v-model="state.marca"
+                >
+                  <option selected value disabled>Select a brand</option>
+                  <option
+                    v-for="option in marcas"
+                    v-bind:value="option.value"
+                    v-bind:key="option"
+                  >
+                    {{ option.text }}
+                  </option>
+                </select>
+                <span class="error" v-if="v$.marca.$error">
+                  {{ v$.marca.$errors[0].$message }}
+                </span>
+              </div>
+              <div class="row">
+                <div class="form-group mb-3 col-xs-12 col-sm-6">
+                  <label for="price">Price €</label>
+                  <input
+                    id="price"
+                    name="price"
+                    v-model="state.precio"
+                    type="number"
+                    min="1"
+                    step="0.01"
+                    class="form-control validate hasDatepicker"
+                  />
+                  <span class="error" v-if="v$.precio.$error">
+                    {{ v$.precio.$errors[0].$message }}
+                  </span>
                 </div>
-                <div class="form-group mb-3">
-                  <label for="marca">Marca</label>
-                  <select class="custom-select tm-select-accounts" v-model="marca">
-                    <option selected disabled>Seleccione una marca</option>
-                    <option
-                      v-for="option in marcas"
-                      v-bind:value="option.value"
-                      v-bind:key="option"
-                    >
-                      {{ option.text }}
-                    </option>
-                  </select>
-                </div>
-                <div class="row">
-                  <div class="form-group mb-3 col-xs-12 col-sm-6">
-                    <label for="expire_date">Precio €</label>
-                    <input
-                      id="expire_date"
-                      name="expire_date"
-                      v-model="precio"
-                      type="number"
-                      min="1"
-                      step="0.01"
-                      class="form-control validate hasDatepicker"
-                    />
-                  </div>
-                  <div class="form-group mb-3 col-xs-12 col-sm-6">
-                    <label for="stock">Stock</label>
-                    <input
-                      id="stock"
-                      name="stock"
-                      type="number"
-                      min="1"
-                      class="form-control validate"
-                      v-model="stock"
-                    />
-                  </div>
+                <div class="form-group mb-3 col-xs-12 col-sm-6">
+                  <label for="stock">Stock</label>
+                  <input
+                    id="stock"
+                    name="stock"
+                    type="number"
+                    min="1"
+                    class="form-control validate"
+                    v-model="state.stock"
+                  />
+                  <span class="error" v-if="v$.stock.$error">
+                    {{ v$.stock.$errors[0].$message }}
+                  </span>
                 </div>
               </div>
             </div>
@@ -127,7 +147,7 @@
                 class="btn btn-primary btn-block text-uppercase"
                 @click="add()"
               >
-                Añadir producto
+                CREATE NEW PRODUCT
               </button>
             </div>
           </form>
@@ -135,16 +155,18 @@
       </div>
     </div>
   </div>
-  <Page404 v-else/> 
+  <Page404 v-else />
 </template>
 
 <script lang="ts" setup>
 import Page404 from "@/components/Page404.vue";
 import { RolUser } from "@/core/general/RolUser";
 import { RoutePaths } from "@/core/general/RoutePaths";
+import useVuelidate from "@vuelidate/core";
+import { helpers, required, minLength, minValue } from "@vuelidate/validators";
 import axios from "axios";
 import Swal from "sweetalert2";
-import { ref, onBeforeMount, reactive } from "vue";
+import { ref, onBeforeMount, reactive, computed } from "vue";
 import { useStore } from "vuex";
 
 /*Vista utilizada para mostrar la función principal de un Gestor de productos, que es poder subir
@@ -154,21 +176,75 @@ un producto con sus características correspondientes*/
 const store = useStore();
 //#endregion
 
-//#region REF
-const nombre = ref<String>("");
-const precio = ref<Number>(1);
-const stock = ref<Number>(1);
-const imagen = ref<any>(null);
-const descripcion = ref<any>("");
+const state = reactive({
+  nombre: "",
+  precio: 1,
+  stock: 1,
+  descripcion: "",
+  categoria: "",
+  tipo: "",
+  marca: "",
+});
 
-const categoria = ref<any>("Seleccione una categoría");
+//#region REF
+// const nombre = ref<String>("");
+// const precio = ref<Number>(1);
+// const stock = ref<Number>(1);
+const imagen = ref<any>(null);
+// const descripcion = ref<any>("");
+
+// const categoria = ref<any>("Seleccione una categoría");
 const categorias = reactive<Array<any>>([]);
 
-const tipo = ref<any>("Seleccione un tipo");
+// const tipo = ref<any>("Seleccione un tipo");
 const tipos = reactive<Array<any>>([]);
 
-const marca = ref<any>("Seleccione una marca");
+// const marca = ref<any>("Seleccione una marca");
 const marcas = reactive<Array<any>>([]);
+//#endregion
+
+//#region VALIDATE
+const rules = computed(() => {
+  return {
+    nombre: {
+      required: helpers.withMessage("*Indique un nombre", required),
+    },
+    descripcion: {
+      required: helpers.withMessage(
+        "*Indique una breve descripción sobre el producto",
+        required
+      ),
+    },
+    categoria: {
+      required: helpers.withMessage(
+        "*Indique a que categoría pertenece",
+        required
+      ),
+    },
+    tipo: {
+      required: helpers.withMessage("*Indique a que tipo pertenece", required),
+    },
+    marca: {
+      required: helpers.withMessage("*Indique a que marca pertenece", required),
+    },
+    precio: {
+      required,
+      minValue: helpers.withMessage(
+        "*El valor debe ser 0.01 o mayor a este",
+        minValue(0.01)
+      ),
+    },
+    stock: {
+      required,
+      minValue: helpers.withMessage(
+        "*El valor debe ser 1 o mayor a este",
+        minValue(1)
+      ),
+    },
+  };
+});
+
+const v$ = useVuelidate(rules, state);
 //#endregion
 
 onBeforeMount(() => {
@@ -183,97 +259,118 @@ const add = async () => {
 };
 
 async function axiosUpload() {
-  await axios
-    .post(`${RoutePaths.API}addProducto.php`, null, {
-      params: {
-        nombre: nombre.value,
-        descripcion: descripcion.value,
-        precio: precio.value,
-        categoria: categoria.value,
-        tipo: tipo.value,
-        marca: marca.value,
-        stock: stock.value,
-      },
-    })
-    .then((response) => {
-      switch (response.status) {
-        case 200:
-          if (response.data) {
-            let formData = new FormData();
-            for (let i = 0; i < imagen.value.files.length; i++) {
-              formData.append("files[]", imagen.value.files[i]);
-            }
-            axios
-              .post(`${RoutePaths.API}addFotosProducto.php`, formData, {
-                headers: {
-                  "Content-Type": "multipart/form-data",
-                },
-              })
-              .then((response) => {
-                switch (response.status) {
-                  case 200:
-                    if (response.data) {
-                      Swal.fire({
-                        icon: "success",
-                        title: "Nuevo producto añadido",
-                        text: "Se ha añadido correctamente el nuevo producto",
-                        showConfirmButton: false,
-                        timer: 1000,
-                      });
-                    }
-                    break;
-
-                  case 404:
-                    Swal.fire({
-                      icon: "error",
-                      title: "ERROR",
-                      text: "Error interno. No se ha encontrado la ruta",
-                      showConfirmButton: false,
-                      timer: 2000,
-                    });
-                    break;
-
-                  case 500:
-                    Swal.fire({
-                      icon: "error",
-                      title: "ERROR",
-                      text: "Error interno. Fallo de API",
-                      showConfirmButton: false,
-                      timer: 2000,
-                    });
-                    break;
+  v$.value.$validate();
+  if (!v$.value.$error) {
+    if (imagen.value.files.lenght > 0) {
+      await axios
+        .post(`${RoutePaths.API}addProducto.php`, null, {
+          params: {
+            nombre: state.nombre,
+            descripcion: state.descripcion,
+            precio: state.precio,
+            categoria: state.categoria,
+            tipo: state.tipo,
+            marca: state.marca,
+            stock: state.stock,
+          },
+        })
+        .then((response) => {
+          switch (response.status) {
+            case 200:
+              if (response.data) {
+                let formData = new FormData();
+                for (let i = 0; i < imagen.value.files.length; i++) {
+                  formData.append("files[]", imagen.value.files[i]);
                 }
-              })
-              .catch((error) => {
-                console.error("There was an error!", error);
+                axios
+                  .post(`${RoutePaths.API}addFotosProducto.php`, formData, {
+                    headers: {
+                      "Content-Type": "multipart/form-data",
+                    },
+                  })
+                  .then((response) => {
+                    switch (response.status) {
+                      case 200:
+                        if (response.data) {
+                          Swal.fire({
+                            icon: "success",
+                            title: "Nuevo producto añadido",
+                            text: "Se ha añadido correctamente el nuevo producto",
+                            showConfirmButton: false,
+                            timer: 1000,
+                          });
+                        }
+                        break;
+
+                      case 404:
+                        Swal.fire({
+                          icon: "error",
+                          title: "ERROR",
+                          text: "Error interno. No se ha encontrado la ruta",
+                          showConfirmButton: false,
+                          timer: 2000,
+                        });
+                        break;
+
+                      case 500:
+                        Swal.fire({
+                          icon: "error",
+                          title: "ERROR",
+                          text: "Error interno. Fallo de API",
+                          showConfirmButton: false,
+                          timer: 2000,
+                        });
+                        break;
+                    }
+                  })
+                  .catch((error) => {
+                    console.error("There was an error!", error);
+                  });
+              }
+              break;
+
+            case 404:
+              Swal.fire({
+                icon: "error",
+                title: "ERROR",
+                text: "Error interno. No se ha encontrado la ruta",
+                showConfirmButton: false,
+                timer: 2000,
               });
+              break;
+
+            case 500:
+              Swal.fire({
+                icon: "error",
+                title: "ERROR",
+                text: "Error interno. Fallo de API",
+                showConfirmButton: false,
+                timer: 2000,
+              });
+              break;
           }
-          break;
-
-        case 404:
-          Swal.fire({
-            icon: "error",
-            title: "ERROR",
-            text: "Error interno. No se ha encontrado la ruta",
-            showConfirmButton: false,
-            timer: 2000,
-          });
-          break;
-
-        case 500:
-          Swal.fire({
-            icon: "error",
-            title: "ERROR",
-            text: "Error interno. Fallo de API",
-            showConfirmButton: false,
-            timer: 2000,
-          });
-          break;
-      }
-    })
-    .catch((error) => {
-      console.error("There was an error!", error);
+        })
+        .catch((error) => {
+          console.error("There was an error!", error);
+        });
+    } else {
+      Swal.fire({
+        icon: "warning",
+        title: "ERROR",
+        text: "El producto debe tener una imagen como mínimo",
+        showConfirmButton: false,
+        timer: 2500,
+      });
+    }
+  } else {
+    Swal.fire({
+      icon: "warning",
+      title: "ERROR",
+      text: "Compruebe que todos los campos seas correctos",
+      showConfirmButton: false,
+      timer: 2500,
     });
+  }
 }
 
 async function getCategorias() {
@@ -424,7 +521,6 @@ async function getMarcas() {
   margin-bottom: 30px;
 }
 
-
 .form-control {
   background-color: #54657d;
   color: #fff;
@@ -478,7 +574,7 @@ async function getMarcas() {
   background-color: rgba(255, 255, 255, 0.05);
 }
 
-.text-color-titles{
+.text-color-titles {
   color: rgb(243, 243, 243);
   font-weight: bold;
 }
@@ -665,5 +761,9 @@ async function getMarcas() {
   #lineChart {
     max-height: 350px;
   }
+}
+
+.error {
+  color: red;
 }
 </style>
